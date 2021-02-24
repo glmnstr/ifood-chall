7997ad6e764f1330fd1c397a9a1855da05e14450
glmnstr <47500627+glmnstr@users.noreply.github.com>
2021-02-24 16:14:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def index():
 	#cidade = request.form.get("cidade")
 	print(request.remote_addr)
 	e = request.args.get("cidade")
-	if (session["tokens"]):
+	if (session):
 		users = cl.profile(session["tokens"].get("access_token"))
 
 	if e == None:
